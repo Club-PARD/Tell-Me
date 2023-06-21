72845b9ad2146b3e396d0a531274a9698359f165
K_Minhyeok <61306657+Kim-Min-Hyeok@users.noreply.github.com>
2023-06-21 12:12:27 +0900

    cancel

--- tell_me/android/app/src/main/kotlin/com/example/tell_me/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tell_me
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- tell_me/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:tell_me/app.dart';
-
-void main() {
-  runApp(const TellMe());
-}
-
