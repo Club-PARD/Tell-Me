e7f704d0b9666e870ea8615ecfb6751349d4be8f
Unduck <csc4592@gmail.com>
2023-06-27 16:18:29 +0900

    [ADD] playlistView.dart 플레이리스트 뷰 추가

---
+++ lib/view/playlistView.dart
@@ -0,0 +1,24 @@
+// import 'package:flutter/material.dart';
+
+// class PlaylistView extends StatefulWidget {
+//   const PlaylistView({super.key});
+
+//   @override
+//   State<PlaylistView> createState() => _PlaylistViewState();
+// }
+
+// class _PlaylistViewState extends State<PlaylistView> {
+//   late VideoItems response;
+//   bool isLoading = false;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         centerTitle: true,
+//         title: const Text('Youtube'),
+//       ),
+//       body: getPage()/일,
+//     );
+//   }
+// }
