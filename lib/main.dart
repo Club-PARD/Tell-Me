12cf98eb21fe6f8da8cd12d6357f36193025e34c
Unduck <csc4592@gmail.com>
2023-06-27 16:44:35 +0900

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
+
+  if (Platform.isMacOS) {
+    final windowManager = WindowManager.instance;
+    windowManager.setMinimumSize(const Size(1650, 1600));
+    windowManager.setMaximumSize(const Size(1650, 1600));
+  }
 }
 
 class MyApp extends StatelessWidget {
