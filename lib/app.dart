33dbdcc0f879e3c4321c388124697dce30b195b9
I-mYejin <iamyejin0318@gmail.com>
2023-06-28 16:13:29 +0900

    [ADD]app.dart에 routes 추가

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:dlive/screens/core_music_add.dart';
+import 'package:dlive/screens/core_music_add_select.dart';
+import 'package:dlive/screens/make_room_qr.dart';
 import 'package:dlive/screens/room_list_screen.dart';
 import 'package:dlive/screens/make_room.dart';
 import 'package:flutter/material.dart';
@@ -11,11 +13,13 @@ class DLive extends StatelessWidget {
     return MaterialApp(
       title: 'DLive',
       debugShowCheckedModeBanner: false,
-      initialRoute: '/makeroom',
+      initialRoute: '/coremusicaddselect',
       routes: {
         '/': (context) => const RoomListScreen(),
         '/makeroom': (context) => const MakeRoom(),
+        '/makeroomqr': (context) => const MakeRoomQrScreen(),
         '/coremusicadd': (context) => const CoreMusicAdd(),
+        '/coremusicaddselect': (context) => const CoreMusicAddSelectScreen(),
       },
     );
   }
