3a15cdcfb2beaf2d61023b917446ac13b8c4dffe
Kim-Min-Hyeok <kkmin11203@gmail.com>
2023-07-02 14:43:05 +0900

    splsh-로그인-홈화면 ui 수정 및 firebase연결
    models: host_model
    screens: splash_page, login_page, signup_page123, home_page 수정(navigation bar 제외)

---
+++ lib/models/host_model.dart
@@ -0,0 +1,13 @@
+class Host {
+  late final String name;
+  late final String email;
+  late final String character;
+  late final List room;
+
+  Host({
+    required this.name,
+    required this.email,
+    required this.character,
+    required this.room,
+  });
+}
--- lib/screens/signup_page1.dart
+++ lib/screens/signup_page1.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:dlive/constants.dart';
 import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
+import 'package:dlive/utils/host_util.dart';
+import 'package:dlive/models/host_model.dart';
+import 'package:provider/provider.dart';
 
-class SignUpPage1 extends StatelessWidget {
+class SignUpPage1 extends StatefulWidget {
   const SignUpPage1({super.key});
 
   @override
+  State<SignUpPage1> createState() => _SignUpPage1State();
+}
+
+class _SignUpPage1State extends State<SignUpPage1> {
+  @override
   Widget build(BuildContext context) {
+    final hostProvider = Provider.of<HostProvider>(context);
+
+    List<String> character = [
+      'assets/character_pale.png',
+      'assets/character_pink.png',
+      'assets/character_yellow.png',
+      'assets/character_green.png',
+      'assets/character_blue.png',
+    ];
+
+    String selectedCharacter = character[0];
+
     return Scaffold(
-      backgroundColor: ColorStyle.background,
+      backgroundColor: Colors.white,
       body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
         SizedBox(
           height: MediaQuery.of(context).size.height / 812 * 140,
@@ -17,55 +37,28 @@ class SignUpPage1 extends StatelessWidget {
           '당신을 대표할 캐릭터를 골라주세요',
           textAlign: TextAlign.center,
           style: TextStyle(
-              fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),
+              fontSize: 18, color: Colors.black, fontWeight: FontWeight.bold),
         ),
         SizedBox(
-          height: MediaQuery.of(context).size.height / 812 * 66,
+          height: MediaQuery.of(context).size.height / 812 * 58,
         ),
-        // SizedBox(
-        //   height: MediaQuery.of(context).size.width / 375 * 240,
-        //   child: ListView.builder(
-        //     scrollDirection: Axis.horizontal,
-        //     itemCount: 5,
-        //     itemExtent: MediaQuery.of(context).size.width / 375 * 240,
-        //     physics: PageScrollPhysics(), // 페이지별 스크롤을 위한 설정
-        //     itemBuilder: (context, index) {
-        //       return Row(
-        //         mainAxisAlignment: MainAxisAlignment.center,
-        //         children: [
-        //           ClipOval(
-        //             child: Container(
-        //               color: Color(0xffD9D9D9),
-        //               height: MediaQuery.of(context).size.width / 375 * 240,
-        //               width: MediaQuery.of(context).size.width / 375 * 240,
-        //             ),
-        //           ),
-        //           SizedBox(width: 10), // ClipOval 사이의 간격 조정
-        //           Text('캐릭터$index'),
-        //         ],
-        //       );
-        //     },
-        //   ),
-        // ),
         ExpandableCarousel(
           options: CarouselOptions(
-            autoPlay: false,
-            showIndicator: false,
-          ),
-          items: [1, 2, 3, 4].map((i) {
+              autoPlay: false,
+              showIndicator: false,
+              onPageChanged: (index, reason) {
+                hostProvider.setCharacter(character[index]);
+                print(hostProvider.character);
+              }),
+          items: [0, 1, 2, 3, 4].map((i) {
             return Builder(
               builder: (BuildContext context) {
                 return Padding(
                   padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-                  child: ClipOval(
-                    child: AspectRatio(
-                      aspectRatio: 1.0,
-                      child: Container(
-                        color: Color(0xffD9D9D9),
-                        height: MediaQuery.of(context).size.width / 375 * 240,
-                        width: MediaQuery.of(context).size.width / 375 * 240,
-                      ),
-                    ),
+                  child: Image.asset(
+                    character[i],
+                    width: MediaQuery.of(context).size.width / 375 * 279,
+                    height: MediaQuery.of(context).size.height / 812 * 280,
                   ),
                 );
               },
@@ -73,21 +66,21 @@ class SignUpPage1 extends StatelessWidget {
           }).toList(),
         ),
         SizedBox(
-          height: MediaQuery.of(context).size.height / 812 * 177,
+          height: MediaQuery.of(context).size.height / 812 * 145,
         ),
         GestureDetector(
           child: Container(
             height: MediaQuery.of(context).size.height / 812 * 46,
             width: MediaQuery.of(context).size.width / 375 * 249,
             decoration: const BoxDecoration(
-                color: Colors.white,
+                color: Colors.black,
                 borderRadius: BorderRadius.all(Radius.circular(8))),
             child: Center(
               child: Text(
                 '확인',
                 style: TextStyle(
                     fontSize: 16,
-                    color: Colors.black,
+                    color: Colors.white,
                     fontWeight: FontWeight.bold),
               ),
             ),
