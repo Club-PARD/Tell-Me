bf516d1b4995cdc56ad5924908754b3679f29ea8
I-mYejin <iamyejin0318@gmail.com>
2023-07-03 18:39:16 +0900

    [UPDATE]make_room_qr.dart QR코드 값 연결

--- lib/screens/core_music_add_select.dart
+++ lib/screens/core_music_add_select.dart
@@ -40,7 +40,7 @@ class _CustomWidgetState extends State<CustomWidget> {
             ),
             Container(
               //이미지 들어갈 자리
-              width: MediaQuery.of(context).size.height / 7.5,
+              width: MediaQuery.of(context).size.height / 7,
               height: MediaQuery.of(context).size.height / 10.5,
               color: Colors.grey,
             ),
@@ -51,39 +51,39 @@ class _CustomWidgetState extends State<CustomWidget> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  "곡 제목",
+                  "노래 명",
                   style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
                   ),
                 ),
                 Text(
-                  "노래 | 가수 이름 | 노래 길이",
+                  "아티스트 명",
                   style: TextStyle(
                     fontSize: 14,
-                    fontWeight: FontWeight.w100,
+                    fontWeight: FontWeight.normal,
                     color: Color(0xFFAEAEAE),
                   ),
                 ),
               ],
             ),
             SizedBox(
-              width: MediaQuery.of(context).size.height / 20,
+              width: MediaQuery.of(context).size.height / 6,
             ),
             Container(
               width: 25,
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
                 border: Border.all(
-                  color: _isChecked ? Colors.green : Colors.grey,
+                  color: _isChecked ? Color(0xFFDE3B15) : Color(0XFFAEAEAE),
                   width: 1,
                 ),
-                color: _isChecked ? Colors.green : Colors.transparent,
+                color: _isChecked ? Color(0xFFDE3B15) : Colors.transparent,
               ),
               child: Checkbox(
                 value: _isChecked,
-                activeColor: Colors.green,
+                activeColor: Color(0xFFDE3B15),
                 fillColor: MaterialStateProperty.all<Color>(Colors.transparent),
                 checkColor: Colors.white,
                 shape: CircleBorder(),
@@ -124,7 +124,7 @@ class _CoreMusicAddSelectScreenState extends State<CoreMusicAddSelectScreen> {
 
   // 포기할 수 없는 3곡(밑에서 슝 올라오는거)
   void _showModalSheet(int index) {
-    Timer(Duration(seconds: 1), () {
+    Timer(Duration(seconds: 2), () {
       Navigator.pop(context); // 모달 닫기
     });
     showModalBottomSheet(
@@ -135,16 +135,16 @@ class _CoreMusicAddSelectScreenState extends State<CoreMusicAddSelectScreen> {
           borderRadius: BorderRadius.vertical(top: Radius.circular(50.0)),
           child: Container(
             height: 270,
-            color: Colors.white,
+            color: Colors.black,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  '포기할 수 없는 3곡을 골라주세요!',
+                  '꼭 듣고 싶은 3곡을 골라주세요!',
                   style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white,
                   ),
                 ),
                 SizedBox(height: MediaQuery.of(context).size.height / 20),
@@ -157,12 +157,13 @@ class _CoreMusicAddSelectScreenState extends State<CoreMusicAddSelectScreen> {
                       height: 80,
                       decoration: BoxDecoration(
                         shape: BoxShape.circle,
-                        color: isOutlineVisibleList[innerIndex]
-                            ? Color(0XFFD9D9D9)
-                            : Color(0XFFD9D9D9),
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/Black_logo.png'),
+                          fit: BoxFit.cover,
+                        ),
                         border: Border.all(
                           color: isOutlineVisibleList[innerIndex]
-                              ? Colors.green
+                              ? Color(0xFFDE3B15)
                               : Color(0XFFD9D9D9),
                           width: 4,
                         ),
@@ -203,108 +204,142 @@ class _CoreMusicAddSelectScreenState extends State<CoreMusicAddSelectScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: const Text(
-          "내 코어 음악 추가",
-          style: TextStyle(
-            color: Colors.white,
-          ),
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+        fit: BoxFit.cover,
+        image: AssetImage(
+          'assets/background.jpg',
         ),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height / 100,
+      )),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.black,
             ),
-            Container(
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.grey.shade700,
-                  hintText: '  가수 이름',
-                  hintStyle: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w100,
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  contentPadding: EdgeInsets.symmetric(
-                    vertical: MediaQuery.of(context).size.height / 17 / 2 - 14,
-                    horizontal: 10,
-                  ),
-                  suffixIcon: Icon(
-                    Icons.search,
-                    color: Colors.white,
-                  ),
-                ),
-                style: TextStyle(
-                  color: Colors.white,
-                ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          title: const Text(
+            "내 음악 추가",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 100,
               ),
-              width: MediaQuery.of(context).size.height / 2.2,
-              height: MediaQuery.of(context).size.height / 17,
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height / 50,
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: isSelectedList.length,
-                itemBuilder: (context, index) {
-                  return CustomWidget(
-                    isChecked: isSelectedList[index],
-                    onChanged: (value) {
-                      if (value!) {
-                        _showModalSheet(index);
-                      }
-                      setState(() {
-                        isSelectedList = List.filled(10, false);
-                        isSelectedList[index] = value;
-                        // isOutlineVisibleList = List.filled(10, false);
-                        isOutlineVisibleList[index] = value;
-
-                        // 완성 버튼 표시 여부 확인
-                        int selectedCount = 0;
-                        for (int i = 0; i < isSelectedList.length; i++) {
-                          if (isSelectedList[i]) {
-                            selectedCount++;
-                          }
-                        }
-                        isCompleteButtonVisible = selectedCount == 3;
-                      });
-                    },
-                  );
-                },
-              ),
-            ),
-            if (isCompleteButtonVisible)
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/makeroomqr');
-                },
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(280, 45),
-                  primary: Color(0XFFCCCCCC),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
+              Container(
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.transparent,
+                    hintText: '곡, 앨범, 아티스트 명 등등의 텍스트',
+                    hintStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFFEEEEEE)),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical:
+                          MediaQuery.of(context).size.height / 17 / 2 - 14,
+                      horizontal: 10,
+                    ),
+                    suffixIcon: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                          Icons.clear,
+                          color: Color(0xFFD8D8D8),
+                        ),
+                        Icon(
+                          Icons.search,
+                          color: Color(0xFF9C9C9C),
+                        ),
+                        SizedBox(
+                            width: MediaQuery.of(context).size.height / 50),
+                      ],
+                    ),
                   ),
-                ),
-                child: const Text(
-                  '완성!',
                   style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
+                width: MediaQuery.of(context).size.height / 2.05,
+                height: MediaQuery.of(context).size.height / 17,
               ),
-          ],
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 50,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: isSelectedList.length,
+                  itemBuilder: (context, index) {
+                    return CustomWidget(
+                      isChecked: isSelectedList[index],
+                      onChanged: (value) {
+                        if (value!) {
+                          _showModalSheet(index);
+                        }
+                        setState(() {
+                          isSelectedList = List.filled(10, false);
+                          isSelectedList[index] = value;
+                          // isOutlineVisibleList = List.filled(10, false);
+                          isOutlineVisibleList[index] = value;
+
+                          // 완성 버튼 표시 여부 확인
+                          int selectedCount = 0;
+                          for (int i = 0; i < isSelectedList.length; i++) {
+                            if (isSelectedList[i]) {
+                              selectedCount++;
+                            }
+                          }
+                          isCompleteButtonVisible = selectedCount == 3;
+                        });
+                      },
+                    );
+                  },
+                ),
+              ),
+              // if (isCompleteButtonVisible)
+              //   ElevatedButton(
+              //     onPressed: () {
+              //       Navigator.pushNamed(context, '/makeroomqr');
+              //     },
+              //     style: ElevatedButton.styleFrom(
+              //       fixedSize: const Size(280, 45),
+              //       primary: Color(0XFFCCCCCC),
+              //       shape: RoundedRectangleBorder(
+              //         borderRadius: BorderRadius.circular(8),
+              //       ),
+              //     ),
+              //     child: const Text(
+              //       '완성!',
+              //       style: TextStyle(
+              //         color: Colors.black,
+              //         fontWeight: FontWeight.bold,
+              //       ),
+              //     ),
+              //   ),
+            ],
+          ),
         ),
       ),
     );
