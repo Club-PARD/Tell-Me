6e1e74ca7e1e4143dc791b6f5bb096b86c41a9cc
I-mYejin <iamyejin0318@gmail.com>
2023-07-05 22:23:16 +0900

    [ADD]Room firebase 연결

--- lib/utils/room_util.dart
+++ lib/utils/room_util.dart
@@ -5,22 +5,27 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class RoomProvider extends ChangeNotifier {
   String _name = '';
+  String _id = '';
   String _img =
       'https://firebasestorage.googleapis.com/v0/b/pard-dlive-b27d9.appspot.com/o/room_img%2Froom_default_color.png?alt=media&token=22258b36-f315-4bc5-b159-f7e73f98baba';
   String _url = '';
   String _playlist = '';
   List _member = [];
-  String _id = ''; //room ID 추가
 
   String get name => _name;
+  String get id => _id;
   String get img => _img;
   String get url => _url;
   String get playlist => _playlist;
   List get member => _member;
-  String get id => _id;
 
   void setName(String name) {
     _name = name;
+    notifyListeners();
+  }
+
+  void setId(String id) {
+    _id = id;
     notifyListeners();
   }
 
@@ -43,19 +48,14 @@ class RoomProvider extends ChangeNotifier {
     _member = member;
     notifyListeners();
   }
-
-  void setId(String id) {
-    _id = id;
-    notifyListeners();
-  }
 }
 
 class RoomUtil {
   final FirebaseAuth auth = FirebaseAuth.instance;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  Future<void> addHost(
-      String name, String img, String url, String playlist, List member) async {
+  Future<void> addRoom(String name, String id, String img, String url,
+      String playlist, List member) async {
     User? user = auth.currentUser;
     CollectionReference hostCollection = firestore.collection('Host');
     DocumentReference hostDocument = hostCollection.doc(user!.uid);
@@ -63,6 +63,7 @@ class RoomUtil {
     CollectionReference roomCollection = firestore.collection('Room');
     DocumentReference newRoomDocument = await roomCollection.add({
       'name': name,
+      'id': id,
       'img': img,
       'url': url,
       'playlist': playlist,
@@ -84,6 +85,7 @@ class RoomUtil {
         Map<String, dynamic> roomData =
             roomSnapshot.data() as Map<String, dynamic>;
         String name = roomData['name'];
+        String id = roomData['id'];
         String img = roomData['img'];
         String url = roomData['url'];
         String playlist = roomData['playlist'];
@@ -91,6 +93,7 @@ class RoomUtil {
 
         Room room = Room(
           name: name,
+          id: id,
           img: img,
           url: url,
           playlist: playlist,
