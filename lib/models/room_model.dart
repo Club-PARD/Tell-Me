0a790c3d4e1aaa959bf646a672016db9627ffeb2
Unduck <csc4592@gmail.com>
2023-07-07 23:07:22 +0900

    [ADD] videoTitles 추가

--- lib/models/room_model.dart
+++ lib/models/room_model.dart
@@ -3,15 +3,17 @@ class Room {
   late String id;
   late String img;
   late String url;
-  // late String playlist;
+  late String playlist;
   late List member;
+  late List videoTitles;
 
   Room({
     required this.name,
     required this.img,
     required this.url,
-    // required this.playlist,
+    required this.playlist,
     required this.member,
+    required this.videoTitles,
     required String id,
   });
 }
